2b6f16268ee000104300e99c445b98cb90785b96
yohei <cha.chu.you.program@gmail.com>
2020-09-08 22:34:12 +0900

    first commit

---
+++ OGY/Project/src/beans/UserTitleBeans.java
@@ -0,0 +1,25 @@
+package beans;
+
+import java.io.Serializable;
+
+public class UserTitleBeans implements Serializable {
+	private int id;
+	private String title;
+
+	public UserTitleBeans(int id, String title) {
+		this.id = id;
+		this.title = title;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
